1b44dfa262a87179e365245894d9dcbd76b275cb
Verglass <85168081+Verglass@users.noreply.github.com>
2023-07-06 22:59:44 +0200

    test

--- server.js
+++ server.js
@@ -53,8 +53,7 @@ app.post('/cart/checkout', cartController.handleCheckout)
 app.post('/cart/checkout/confirm', cartController.handleConfirm)
 
 app.listen(PORT, () => { 
-    console.log({`Listening on port ${PORT}`)
-    console.log(3)        
+    console.log(`Listening on port ${PORT}`)
     console.log(process.env)
     console.log(process.env.DATABASE_URI)  
 )
